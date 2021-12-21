14509c08ddb551238b08952fe55a58fa28afbc44
Liurx <liurongxin_i@didiglobal.com>
2021-12-21 22:50:01 +0800

    fix: process not defined

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoLessPlugin = require("craco-less");
+const webpack = require("webpack");
 
 module.exports = {
   devServer: {
@@ -11,6 +12,13 @@ module.exports = {
         },
       },
     },
+  },
+  webpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      }),
+    ],
   },
   plugins: [
     {
